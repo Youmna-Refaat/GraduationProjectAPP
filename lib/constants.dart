1a31cf70b5ef8a2f32401bcc4bf4bacbb57f32b7
Youmna-Refaat <youmnarefaat884@gmail.com>
2024-11-27 00:08:28 +0200

    firebase config and contstants file

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff607D8B);
+const kTextFieldColor = Color(0xFFC3C3C3);
+const kTextColor = Colors.white;
+const kHintTextColor = Color(0xFF000000); //With opacity 0.6
