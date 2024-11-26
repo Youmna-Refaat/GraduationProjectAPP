091f64fbea8be210aa881b1499988eb67da31c09
Youmna-Refaat <youmnarefaat884@gmail.com>
2024-11-26 23:54:23 +0200

    init commit

---
+++ nativelib/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.nativelib"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        externalNativeBuild {
+            cmake {
+                cppFlags("")
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
+
+dependencies {
+
+}
