3d45738d09dc9fec92eaa8c72c2679fca5489b50
Youmna-Refaat <youmnarefaat884@gmail.com>
2024-11-27 00:41:31 +0200

    location service

---
+++ lib/utils/location_service.dart
@@ -0,0 +1,40 @@
+import 'package:location/location.dart';
+
+class LocationService {
+  Location location = Location();
+
+  Future<bool> checkAndRequestLocationService() async {
+    var isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      isServiceEnabled = await location.requestService();
+      if (!isServiceEnabled) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<bool> checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+    }
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+      return permissionStatus == PermissionStatus.granted;
+    }
+    return true;
+  }
+
+  void getRealTimeLocationData(void Function(LocationData)? onData) async {
+    await checkAndRequestLocationService();
+    await checkAndRequestLocationPermission();
+    location.onLocationChanged.listen(onData);
+  }
+
+  Future<LocationData> getLocation() async {
+    await checkAndRequestLocationService();
+    await checkAndRequestLocationPermission();
+    return await location.getLocation();
+  }
+}
