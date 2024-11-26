4a5591a58f8735e72bc921d25e241d4ca0cb1dbc
Youmna-Refaat <youmnarefaat884@gmail.com>
2024-11-27 00:39:31 +0200

    supervisor bottom appbar

---
+++ lib/widgets/AppBars/supervisor_custom_bottom_app_bar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:graduation_application/constants.dart';
+import 'package:graduation_application/screens/general/register.dart';
+import 'package:graduation_application/screens/child/alerts_supervisor.dart';
+import 'package:graduation_application/screens/child/home_page_supervisor.dart';
+import 'package:graduation_application/widgets/AppBars/bottom_app_bar_items.dart';
+
+class SupervisorBottomAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  final double appBarHeight = 50.0;
+  const SupervisorBottomAppBar({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: kPrimaryColor,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            BottomAppBarItem(
+              icon: FontAwesomeIcons.bell,
+              title: 'Alert',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  _createRoute(const SupervisorAlerts()),
+                );
+              },
+            ),
+            BottomAppBarItem(
+              icon: Icons.home,
+              title: 'Home',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  _createRoute(const SupervisorHomePage()),
+                );
+              },
+            ),
+            BottomAppBarItem(
+              icon: Icons.logout_rounded,
+              title: 'Logout',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  _createRoute(const RegisterPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Route _createRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+}
